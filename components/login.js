fb8e844c0fba007fe561274716c1c5eed8b1761e
qangviet <viet.tq215515@sis.hust.edu.vn>
2024-10-09 14:08:38 +0700

    Edit login

--- components/login.js
+++ components/login.js
@@ -33,60 +33,58 @@ const LoginScreen = () => {
     };
 
     return (
-        <SafeAreaView>
-            <View style={styles.container}>
-                <View style={styles.elevation}>
-                    <Image source={require("../assets/logo.png")} style={styles.logo} />
-                </View>
-                <Text style={styles.title}>Đăng nhập với tài khoản QLDT</Text>
-
-                <StyledView className="relative my-4">
-                    <StyledTextInput
-                        className="bg-transparent text-white border border-white 
-                    rounded-full py-3 px-12 text-base font-medium"
-                        placeholder="Email hoặc mã số SV/CB"
-                        value={email}
-                        onChangeText={setEmail}
-                        placeholderTextColor="#e6e8e6"
-                    />
-                    <View
-                        className="flex-1 justify-center items-center absolute 
-                left-5 top-1/2 -translate-y-3"
-                    >
-                        <Ionicons name="person" size={22} color="white" />
-                    </View>
-                </StyledView>
-                <StyledView className="relative my-3">
-                    <StyledTextInput
-                        className="bg-transparents border border-white text-white 
-                    rounded-full py-3 px-12 text-base font-medium"
-                        placeholder="Mật khẩu"
-                        secureTextEntry
-                        value={password}
-                        onChangeText={setPassword}
-                        placeholderTextColor="#e6e8e6"
-                    />
-                    <View className="absolute top-1/2 -translate-y-[10px] left-5">
-                        <Fontisto name="locked" size={20} color="white" />
-                    </View>
-                    <View className="absolute top-1/2 right-4 -translate-y-[10px]">
-                        <Ionicons name="eye-off-outline" size={20} color="white" />
-                    </View>
-                </StyledView>
-
-                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-                    <Text style={styles.loginButtonText}>ĐĂNG NHẬP</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.fingerprintButton}>
-                    <Ionicons name="finger-print" size={24} color="white" />
-                </TouchableOpacity>
-
-                <TouchableOpacity>
-                    <Text style={styles.forgotPassword}>Quên mật khẩu?</Text>
-                </TouchableOpacity>
+        <View style={styles.container}>
+            <View>
+                <Image source={require("../assets/logo.png")} style={styles.logo} />
             </View>
-        </SafeAreaView>
+            <Text style={styles.title}>Đăng nhập với tài khoản QLDT</Text>
+
+            <StyledView className="relative my-4 w-[85%]">
+                <StyledTextInput
+                    className="bg-transparent text-white border border-white 
+                    rounded-full py-3 px-12 text-base font-medium"
+                    placeholder="Email hoặc mã số SV/CB"
+                    value={username}
+                    onChangeText={setUsername}
+                    placeholderTextColor="#e6e8e6"
+                />
+                <View
+                    className="flex-1 justify-center items-center absolute 
+                left-5 top-1/2 -translate-y-3"
+                >
+                    <Ionicons name="person" size={22} color="white" />
+                </View>
+            </StyledView>
+            <StyledView className="relative my-3 w-[85%]">
+                <StyledTextInput
+                    className="bg-transparents border border-white text-white 
+                    rounded-full py-3 px-12 text-base font-medium"
+                    placeholder="Mật khẩu"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                    placeholderTextColor="#e6e8e6"
+                />
+                <View className="absolute top-1/2 -translate-y-[10px] left-5">
+                    <Fontisto name="locked" size={20} color="white" />
+                </View>
+                <View className="absolute top-1/2 right-4 -translate-y-[10px]">
+                    <Ionicons name="eye-off-outline" size={20} color="white" />
+                </View>
+            </StyledView>
+
+            <TouchableOpacity style={styles.loginButton} className="my-5" onPress={handleLogin}>
+                <Text style={styles.loginButtonText}>ĐĂNG NHẬP</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.fingerprintButton}>
+                <Ionicons name="finger-print" size={24} color="white" />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text style={styles.forgotPassword}>Quên mật khẩu?</Text>
+            </TouchableOpacity>
+        </View>
     );
 };
 
@@ -133,7 +131,7 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         borderRadius: 10,
         padding: 15,
-        width: "90%",
+        width: "85%",
         alignItems: "center",
     },
 
