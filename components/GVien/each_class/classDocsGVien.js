0650a74c118cc21d391c420469732c78ade0975e
LƯỜNG MẠNH TÚ <tu.manhluong3103@gmail.com>
2024-12-14 01:58:51 +0700

    modal comfirm delete doc

--- components/GVien/each_class/classDocsGVien.js
+++ components/GVien/each_class/classDocsGVien.js
@@ -21,14 +21,14 @@ const ClassDocsScreenGVien = ({ route }) => {
       const [class_id, setClassId] = useState("");
       const [DOCs, setDOCs] = useState([]);
       const state = useSelector((state) => state.navigation);
-      
+      const [showConfirmDelete, setShowConfirmDelete] = useState(false);
 
       useEffect(() => {
-		if (state.currentScreen !== "ClassDocsScreenGVien") {
-			navigation.navigate(currentScreen);
-		}
-	}, [currentScreen]);
-      
+            if (state.currentScreen !== "ClassDocsScreenGVien") {
+                  navigation.navigate(currentScreen);
+            }
+      }, [currentScreen]);
+
       // useEffect(() => {
       //       if (params.class != null) {
       //             setToken(params.token);
@@ -38,69 +38,34 @@ const ClassDocsScreenGVien = ({ route }) => {
 
       const fetchDocs = async () => {
             try {
-                console.log(">>>> Token: ", params.token);
-                const response = await api.post('/it5023e/get_material_list', {
-                    token: params.token,
-                    class_id: params.class.id,
-                });
-        
-                if (response.data.code === "1000") {
-                    const fetchedDOCs = response.data.data.map((item) => ({
-                        id: item.id,
-                        material_name: item.material_name,
-                        description: item.description,
-                        material_link: item.material_link,
-                        class_id: item.class_id,
-                        material_type: item.material_type,
-                    }));
-                    setDOCs(fetchedDOCs);
-                } else {
-                    console.error('API error:', response.data.message);
-                }
+                  console.log(">>>> Token: ", params.token);
+                  const response = await api.post('/it5023e/get_material_list', {
+                        token: params.token,
+                        class_id: params.class.id,
+                  });
+
+                  if (response.data.code === "1000") {
+                        const fetchedDOCs = response.data.data.map((item) => ({
+                              id: item.id,
+                              material_name: item.material_name,
+                              description: item.description,
+                              material_link: item.material_link,
+                              class_id: item.class_id,
+                              material_type: item.material_type,
+                        }));
+                        setDOCs(fetchedDOCs);
+                  } else {
+                        console.error('API error:', response.data.message);
+                  }
             } catch (error) {
-                console.error("Error fetching documents:", error);
+                  console.error("Error fetching documents:", error);
             }
-        };
-        
+      };
+
       useEffect(() => {
             fetchDocs();
-            }, [currentScreen]);
-              
-      // useEffect(() => {
-      //       async function fetchDocs() {
-      //             try {
-      //                   console.log(">>>> Token: ", params.token);
-      //                   const response = await api.post('/it5023e/get_material_list', {
-      //                         token: params.token,
-	// 				class_id: params.class.id
-	// 			});
-                        
-	// 			if (response.data.code === "1000") {
-      //                         const fetchedDOCs = response.data.data.map((item) => ({
-      //                               id: item.id,
-	// 					material_name: item.material_name,
-	// 					description: item.description,
-	// 					material_link: item.material_link,
-	// 					material_link: item.material_link,
-	// 					class_id: item.class_id,
-      //                               material_type: item.material_type
-	// 				}));
-	// 				setDOCs(fetchedDOCs);
-	// 			} else {
-      //                         console.error('API error:', response.data.message);
-	// 			}
-	// 		} catch (error) {
-      //                   console.error('Network error:', error);
-	// 			console.error("API call failed: ", error);
-	// 			console.error("Error fetching class list:", error);
-	// 			console.error("Error Data:", error.response.data);
-	// 			console.error("Error Status:", error.response.status);
-	// 		}
-	// 	}
-            
-	// 	fetchDocs();
-	// }, [currentScreen]);
-      
+      }, [currentScreen]);
+
       console.log("DOCS: ", DOCs);
 
       const [modalVisible, setModalVisible] = useState(false);
@@ -110,17 +75,24 @@ const ClassDocsScreenGVien = ({ route }) => {
             setSelectedDoc(item);
             setModalVisible(true);
       };
-
       const handleCloseModal = () => {
             setModalVisible(false);
             setSelectedDoc(null);
       };
 
+      const openModalConfirmDelete = () => {
+            setShowConfirmDelete(true);
+            console.log(showConfirmDelete);
+      };
+      const closeModalConfirmDelete = () => {
+            setShowConfirmDelete(false);
+      };
+
       //const [is_open, setIsOpen] = useState(false);
       const openMaterial = (url) => {
-		console.log("Mo tai lieu...");
-		//setIsOpen(true);
-		
+            console.log("Mo tai lieu...");
+            //setIsOpen(true);
+
             try {
                   Linking.openURL(url).catch((err) => {
                         console.error("Failed to open URL: ", err);
@@ -144,18 +116,18 @@ const ClassDocsScreenGVien = ({ route }) => {
             // );
             dispatch(
                   navigate({
-                      screen: "EditMaterialGVien",
-                      params: {
-                        token: params.token,
-                        doc: doc
-                      }  // Tránh việc truyền undefined
+                        screen: "EditMaterialGVien",
+                        params: {
+                              token: params.token,
+                              doc: doc
+                        }  // Tránh việc truyền undefined
                   })
-              );
-        }
-      
+            );
+      }
+
       const deleteDoc = async (doc) => {
             console.log("Deleting docs....");
-            console.log("DOCS...",doc);
+            console.log("DOCS...", doc);
             //setConfirmSave(false);
             try {
                   const response = await api.post("/it5023e/delete_material", {
@@ -170,12 +142,14 @@ const ClassDocsScreenGVien = ({ route }) => {
                         goBack();
                   } else {
                         alert("Vui lòng thử lại");
+                        setModalVisible(false);
+                        setShowConfirmDelete(false);
                   }
-                  } catch (error) {
-                        console.log("ERROR!")
-                  }
-            };
-            
+            } catch (error) {
+                  console.log("ERROR!")
+            }
+      };
+
 
       const CustomButton = ({ title, onPress, iconName, style }) => (
             <TouchableOpacity
@@ -232,37 +206,68 @@ const ClassDocsScreenGVien = ({ route }) => {
                                           borderTopRightRadius: 20,
                                           padding: 10,
                                           elevation: 5,
-                                          height: 320
+                                          height: 250
                                     }}
                               >
                                     <Text className="text-lg mb-2 self-center">
                                           {selectedDoc ? selectedDoc.name : ''}
                                     </Text>
-                                    <CustomButton 
-                                          title="Mở" 
+                                    <CustomButton
+                                          title="Mở"
                                           onPress={() => {
                                                 openMaterial(selectedDoc.material_link);
                                                 console.log(`Mở ${selectedDoc?.material_name}`);
-                                          }} 
-                                          iconName="open-outline" 
-                                          />
-                                    <CustomButton 
-                                          title="Chỉnh sửa" 
+                                          }}
+                                          iconName="open-outline"
+                                    />
+                                    <CustomButton
+                                          title="Chỉnh sửa"
                                           onPress={() => {
                                                 console.log(`Đổi tên ${selectedDoc?.material_name}`);
                                                 goEditDoc(selectedDoc);
                                           }}
                                           iconName="create-outline" />
-                                    <CustomButton 
-                                          title="Xóa" 
-                                          onPress={() => {
-                                                console.log(`Xóa ${selectedDoc?.material_name}`);
-                                                deleteDoc(selectedDoc);
-                                          }}
+                                    <CustomButton
+                                          title="Xóa"
+                                          onPress={openModalConfirmDelete}
                                           iconName="trash-outline" />
                               </View>
                               {/* </View> */}
                         </View>
+                        {showConfirmDelete && <Modal
+                              transparent={true}
+                              animationType="slide"
+                              onBackdropPress={closeModalConfirmDelete}
+                        >
+                              <View className="flex-1 justify-center items-center bg-transparent bg-opacity-50">
+                                    <View className="bg-gray-100 rounded-md pb-5 w-4/5">
+                                          <View className="flex-row justify-end items-center mt-2 px-3">
+                                                <TouchableOpacity onPress={closeModalConfirmDelete} className="h-9">
+                                                      <Ionicons
+                                                            name="close-outline"
+                                                            size={30}
+                                                            color="gray"
+                                                            className="mt-2 mr-3"
+                                                      />
+                                                </TouchableOpacity>
+                                          </View>
+                                          <View className="border-t border-gray-400 justify-center"></View>
+                                          <View>
+                                                <Text className="text-xl font-bold px-5 py-3 self-center">
+                                                      Xác nhận xóa tài liệu?
+                                                </Text>
+                                          </View>
+                                          <View className="flex flex-row justify-end px-3 gap-x-5 pt-3">
+                                                <TouchableOpacity className="px-3 py-2 bg-blue-500 rounded-lg" onPress={() => deleteDoc(selectedDoc)}>
+                                                      <Text className="text-lg text-gray-300">Xóa</Text>
+                                                </TouchableOpacity>
+                                                <TouchableOpacity className="px-3 py-2 bg-red-600 rounded-lg" onPress={closeModalConfirmDelete}>
+                                                      <Text className="text-white text-lg">Không</Text>
+                                                </TouchableOpacity>
+                                          </View>
+                                    </View>
+                              </View>
+                        </Modal>}
                   </Modal>
             </View >
       )
