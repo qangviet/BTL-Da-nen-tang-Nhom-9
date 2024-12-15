f20de46978ee095bd2524c4e9d5cac5dc90f7349
VuDinhToan303 <thcslequydon9c@gmail.com>
2024-12-15 12:15:17 +0700

    thong bao chat

--- components/SVien/main_screen/chatSVien.js
+++ components/SVien/main_screen/chatSVien.js
@@ -1,17 +1,21 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { LogoHust, LogoBK } from "./../../logo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { navigate } from "../../../redux/navigationSlice";
+import api from "../../api";
+import { useIsFocused } from "@react-navigation/native";
+import { useNavigation as useReactNavigation, useFocusEffect } from "@react-navigation/native";
 
 const ChatSVien = () => {
 	const dispatch = useDispatch();
 	const state = useSelector((state) => state.navigation);
 	const navigation = useNavigation();
+	const param = useSelector((state) => state.navigation.params);
+	console.log("Chat param:....", param);
 
 	useEffect(() => {
 		if (state.currentScreen !== "ChatSVien") {
@@ -23,10 +27,36 @@ const ChatSVien = () => {
 		dispatch(
 			navigate({
 				screen: "NotiSVien",
-				params: {},
+				params: param,
 			})
 		);
 	};
+
+	const [noti_count, setNotiCount] = useState(0);
+
+	const fetchCountNoti = async () => {
+		try {
+		  const response = await api.post("/it5023e/get_unread_notification_count", {
+			token: param.token,
+		  });
+	
+		  if (response.data.meta.code === "1000") {
+			const notiCount = response.data.data;
+			setNotiCount(notiCount);
+		  } else {
+			console.error("API error:", response.data);
+		  }
+		} catch (error) {
+		  console.error("Error fetching notifications:", error?.message || error);
+		}
+	  };
+	
+	  // Tự động gọi API mỗi khi màn hình HomeSVien được focus
+	  useFocusEffect(
+		useCallback(() => {
+		  fetchCountNoti();
+		}, []) // Không cần dependencies vì chỉ chạy khi focus
+	  );
 
 	return (
 		<View className="bg-[#dfe1e2]">
@@ -35,17 +65,20 @@ const ChatSVien = () => {
 					<Text className="text-xl text-white font-semibold">Nhắn tin</Text>
 				</View>
 				<View className="absolute right-4 top-10">
-					<TouchableOpacity onPress={gotoNotification}>
-						<View className="relative">
-							<View
-								className="bg-red-500 rounded-full h-6 w-6 
+				<TouchableOpacity onPress={gotoNotification}>
+							<View className="relative">
+								{noti_count > 0 && (
+									<View
+										className="bg-red-500 rounded-full h-6 w-6 
 								flex justify-center items-center absolute z-10 -right-3 -top-3"
-							>
-								<Text className="text-white text-[11px]">10+</Text>
+									>
+										<Text className="text-white text-[11px]">{noti_count > 10 ? `${noti_count}+` : noti_count}</Text>
+									</View>
+								)}
+
+								<Ionicons name="notifications" size={24} color="white" />
 							</View>
-							<Ionicons name="notifications" size={24} color="white" />
-						</View>
-					</TouchableOpacity>
+						</TouchableOpacity>
 				</View>
 				<View className="absolute left-5 top-14">
 					<LogoBK width={32} height={48} className="mx-auto"></LogoBK>
